53c85f23cddea2372220a475a17b86cf5cbe4bcf
cheeku8 <cheeku99918@gmail.com>
2024-02-16 12:03:53 +0530

    todo

--- todo.js
+++ todo.js
@@ -5,7 +5,37 @@ const reset = document.getElementById("reset");
 const elete = document.getElementById("delete");
 
 let names = [];
+nameInput.addEventListener('keypress', function(event){
+    if(event.key === 'Enter')
+    {
+        event.preventDefault();
 
+        inputButton.click();
+    }
+})
+nameInput.addEventListener('keydown', function(event){
+    if(nameInput.value ===''){
+      
+        if(event.key === 'Backspace')
+         {
+             event.preventDefault();
+
+             elete.click();
+         }
+    }
+})
+
+nameInput.addEventListener('keydown', function(event){
+    if(nameInput.value ===''){
+      
+        if(event.key === 'Escape')
+         {
+             event.preventDefault();
+
+             reset.click();
+         }
+    }
+})
 inputButton.addEventListener('click',() => {
     const name = nameInput.value.trim();
     if(name !== ''){
